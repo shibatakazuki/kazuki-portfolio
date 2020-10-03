366c87d076dc2dd4921037aa0e10e2624a2faaf0
shibatakazuki <decks.909efx@gmail.com>
2020-10-04 06:55:52 +0900

    js

--- js/common.js
+++ js/common.js
@@ -17,3 +17,17 @@ $( function() {
         ]
     } );
 } );
+
+         
+         $(function(){   
+                $(window).scroll(function(){
+              //.l-aboutから.l-skillsの間はこれ
+             if ($(window).scrollTop() > $('.l-about').offset().top &&　$(window).scrollTop() < $('.l-skills').offset().top){
+                  $(".l-header__nav .l-header__nav__list .l-header__nav__list__mobile a").css("color", "#5C5C5C");
+             }//.l-directionより進んだらこれ
+                  else if($(window).scrollTop() > $('.l-direction').offset().top){
+                  $(".l-header__nav .l-header__nav__list .l-header__nav__list__mobile a").css("color", "#5C5C5C");
+             }else//それ以外（l-frontpage l-service l-contactである場合）はこれ
+                  $(".l-header__nav .l-header__nav__list .l-header__nav__list__mobile a").css("color", "#FEFEFE");
+                   });
+             }); 
